795ec22dd1c6c0c8bbd9c32d18a78ff284430442
Ethan Chappel <EthanChappel@users.noreply.github.com>
2022-03-24 17:27:50 -0500

    Check if UTC before calculating offset.

--- date-time-transformation.py
+++ date-time-transformation.py
@@ -7,15 +7,20 @@ if len(sys.argv) < 3:
 
 f = '%Y-%m-%dT%H:%M:%S%z'
 d = datetime.datetime.strptime(sys.argv[1], f)
-d = d.replace(tzinfo=datetime.timezone(datetime.timedelta(hours=int(sys.argv[2])))) + datetime.timedelta(hours=int(sys.argv[2]))
+old_is_utc = d.utcoffset().total_seconds() == 0
+d = d.replace(tzinfo=datetime.timezone(datetime.timedelta(hours=int(sys.argv[2])))) - d.utcoffset() + datetime.timedelta(hours=int(sys.argv[2])) + d.utcoffset()
+new_is_utc = d.utcoffset().total_seconds() == 0
 
 if len(sys.argv) > 4:
     dst_start = datetime.datetime.strptime(sys.argv[3], f)
     dst_end = datetime.datetime.strptime(sys.argv[4], f)
 
     if dst_start < d and d < dst_end:
-        d += datetime.timedelta(hours=1)
-    
+        if old_is_utc:
+            d += datetime.timedelta(hours=1)
+        if new_is_utc:
+            d -= datetime.timedelta(hours=1)
+
 elif len(sys.argv) > 3:
     print('Warning: Not enough arguments to use daylight saving time mode.', file=sys.stderr)
 
