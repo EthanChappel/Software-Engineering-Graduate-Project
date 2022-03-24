4cd2a671fddf9c6b3ac08d819dc5014dea9f1c06
Ethan Chappel <EthanChappel@users.noreply.github.com>
2022-03-24 13:33:57 -0500

    Add support for entering daylight savings time

--- date-time-transformation.py
+++ date-time-transformation.py
@@ -4,4 +4,9 @@ import datetime
 f = "%Y-%m-%d %H:%M:%S %Z%z"
 d = datetime.datetime.strptime(sys.argv[1], f)
 d = d.replace(tzinfo=datetime.timezone(datetime.timedelta(hours=int(sys.argv[2])))) + datetime.timedelta(hours=int(sys.argv[2]))
-print(d.strftime("%Y-%m-%d %H:%M:%S %Z"))
+if len(sys.argv) > 4:
+    dst_start = datetime.datetime.strptime(sys.argv[3], f)
+    dst_end = datetime.datetime.strptime(sys.argv[4], f)
+elif len(sys.argv) > 3:
+    print('Warning: Not enough values present to use daylight saving time mode.', file=sys.stderr)
+
