79b133b08f86b31b6a2d4120c6a614b7b68f2f80
Ethan Chappel <EthanChappel@users.noreply.github.com>
2022-03-24 14:04:49 -0500

    Print to standard error

--- big-number-computation.py
+++ big-number-computation.py
@@ -4,18 +4,18 @@ def to_int(s):
     try:
         return int(s)
     except ValueError:
-        print('Error: \'%s\' is not an integer.' % s)
+        print('Error: \'%s\' is not an integer.' % s, file=sys.stderr)
         sys.exit(1)
 
 if sys.argv[2] != '+' and sys.argv[2] != '-':
-    print('Error: Operator must be \'+\' or \'-\'')
+    print('Error: Operator must be \'+\' or \'-\'', file=sys.stderr)
     sys.exit(2)
 
 a_s = None
 b_s = None
 d_s = None
 if '.' in sys.argv[1] and '.' in sys.argv[3]:
-    print('Error: Only one value can have a decimal.')
+    print('Error: Only one value can have a decimal.', file=sys.stderr)
     sys.exit(3)
 if '.' in sys.argv[1]:
     a_s, d_s = sys.argv[1].split('.')
