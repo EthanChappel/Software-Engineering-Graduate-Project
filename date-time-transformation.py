b17a100a682f0870eb99a3357df2191d22cb69c5
Ethan Chappel <EthanChappel@users.noreply.github.com>
2022-05-06 03:46:52 -0500

    Restructure date time transformation

--- date-time-transformation.py
+++ date-time-transformation.py
@@ -1,32 +1,42 @@
 import sys
 import datetime
 
-if len(sys.argv) < 3:
-    print("Error: Not enough arguments to perform time zone transformation.", file=sys.stderr)
-    sys.exit(1)
-
-if int(sys.argv[2]) < -12 or int(sys.argv[2]) > +14:
-    print('Error: Timezone must be within -12 and +14')
-    sys.exit(2)
-
 
 f = '%Y-%m-%dT%H:%M:%S%z'
-d = datetime.datetime.strptime(sys.argv[1], f)
-old_is_utc = d.utcoffset().total_seconds() == 0
-d = d.replace(tzinfo=datetime.timezone(datetime.timedelta(hours=int(sys.argv[2])))) - d.utcoffset() + datetime.timedelta(hours=int(sys.argv[2]))
-new_is_utc = d.utcoffset().total_seconds() == 0
 
-if len(sys.argv) > 4:
-    dst_start = datetime.datetime.strptime(sys.argv[3], f)
-    dst_end = datetime.datetime.strptime(sys.argv[4], f)
 
-    if dst_start < d and d < dst_end:
-        if old_is_utc:
-            d += datetime.timedelta(hours=1)
-        if new_is_utc:
-            d -= datetime.timedelta(hours=1)
+def transform_datetime(string, offset, dst_start = None, dst_end = None):
+    d = datetime.datetime.strptime(string, f)
+    old_is_utc = d.utcoffset().total_seconds() == 0
+    d = d.replace(tzinfo=datetime.timezone(datetime.timedelta(hours=offset))) - d.utcoffset() + datetime.timedelta(hours=offset)
+    new_is_utc = d.utcoffset().total_seconds() == 0
+    
+    if dst_start != None and dst_end != None:
+        if dst_start < d and d < dst_end:
+            if old_is_utc:
+                d += datetime.timedelta(hours=1)
+            if new_is_utc:
+                d -= datetime.timedelta(hours=1)
+    
+    return d
 
-elif len(sys.argv) > 3:
-    print('Warning: Not enough arguments to use daylight saving time mode.', file=sys.stderr)
+if __name__ == '__main__':
+    if len(sys.argv) < 3:
+        print("Error: Not enough arguments to perform time zone transformation.", file=sys.stderr)
+        sys.exit(1)
 
-print(d.strftime("%s %%A" % f))
+    if int(sys.argv[2]) < -12 or int(sys.argv[2]) > +14:
+        print('Error: Timezone must be within -12 and +14')
+        sys.exit(2)
+
+    elif len(sys.argv) > 5:
+        print('Warning: Not enough arguments to use daylight saving time mode.', file=sys.stderr)
+
+    d = transform_datetime(
+        sys.argv[1],
+        int(sys.argv[2]),
+        datetime.datetime.strptime(sys.argv[3], f) if len(sys.argv) > 4 else None,
+        datetime.datetime.strptime(sys.argv[4], f) if len(sys.argv) > 4 else None
+    )
+
+    print(d.strftime("%s %%A" % f))
