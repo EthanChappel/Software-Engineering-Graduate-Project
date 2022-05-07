d764112a53356c0d0eddff50cd6a9e9c82cdbea0
Ethan Chappel <EthanChappel@users.noreply.github.com>
2022-05-07 03:45:30 -0500

    Move datetime transformation

--- date_time_transformation.py
+++ date_time_transformation.py
@@ -5,14 +5,13 @@ import datetime
 f = '%Y-%m-%dT%H:%M:%S%z'
 
 
-def transform_datetime(string, offset, dst_start = None, dst_end = None):
-    d = datetime.datetime.strptime(string, f)
+def transform_datetime(d, offset, dst_start = None, dst_end = None):
     old_is_utc = d.utcoffset().total_seconds() == 0
     d = d.replace(tzinfo=datetime.timezone(datetime.timedelta(hours=offset))) - d.utcoffset() + datetime.timedelta(hours=offset)
     new_is_utc = d.utcoffset().total_seconds() == 0
     
     if dst_start != None and dst_end != None:
-        if dst_start < d and d < dst_end:
+        if dst_start <= d and d < dst_end:
             if old_is_utc:
                 d += datetime.timedelta(hours=1)
             if new_is_utc:
@@ -33,7 +32,7 @@ if __name__ == '__main__':
         print('Warning: Not enough arguments to use daylight saving time mode.', file=sys.stderr)
 
     d = transform_datetime(
-        sys.argv[1],
+        datetime.datetime.strptime(sys.argv[1], f),
         int(sys.argv[2]),
         datetime.datetime.strptime(sys.argv[3], f) if len(sys.argv) > 4 else None,
         datetime.datetime.strptime(sys.argv[4], f) if len(sys.argv) > 4 else None
