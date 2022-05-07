b97ef507843dd2d77a852512f24ced5a4f34ad27
Ethan Chappel <EthanChappel@users.noreply.github.com>
2022-05-07 03:11:55 -0500

    Add tests for string and words

--- tests.py
+++ tests.py
@@ -1,5 +1,6 @@
 import pytest
 from big_number_computation import big_number_computation, to_int
+from date_time_transformation import transform_datetime
 from string_and_words import word_count, word_replacement, grep_line
 
 
@@ -43,3 +44,29 @@ def test_big_number_computation_big_multiplication():
     assert big_number_computation("200000000000000000000000000000000000000000", "*", "100000000000000000000000000000000000000000") \
         == "20000000000000000000000000000000000000000000000000000000000000000000000000000000000"
 
+
+# String and words
+
+test_text = "The cosmos is all that is...\n...or ever was or ever will be."
+
+def test_word_count():
+    assert word_count(test_text) \
+        == {
+            'The': 1,
+            'cosmos': 1,
+            'is': 2,
+            'all': 1,
+            'that': 1,
+            'or': 2,
+            'ever': 2,
+            'was': 1,
+            'will': 1,
+            'be': 1
+        }
+
+def test_word_replacement():
+    assert word_replacement('cosmos', 'universe', test_text) \
+        == "The universe is all that is...\n...or ever was or ever will be."
+
+def test_grep_line():
+    assert grep_line("cosmos", test_text) == [' 1:  The cosmos is all that is...']
